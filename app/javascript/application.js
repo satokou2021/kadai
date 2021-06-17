18ae670c3be163ebaeb199ca362a05a6a3c3a108
ko <gityoudayo.gmail.com>
2021-06-17 09:38:19 +0900

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+document.addEventListener('turbolinks:load', () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const links = document.querySelectorAll(".nav-links li");
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+      link.classList.toggle("fade");
+    });
+  });
+});
